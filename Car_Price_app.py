83cbe9498225c04ca26256d8c7b324baa8217998
Saurabh-Singh68 <saurabhew09@gmail.com>
2026-06-16 09:50:06 +0530

    Update Car_Price_app.py

--- Car_Price_app.py
+++ Car_Price_app.py
@@ -4,27 +4,50 @@ import pickle
 
 model = pickle.load(open("LinearRegressionModel.pkl", "rb"))
 
-st.title("🚗 Car Price Predictor")
+car_names = pickle.load(open("car_names.pkl", "rb"))
+companies = pickle.load(open("companies.pkl", "rb"))
 
-name = st.text_input("Car Name")
-company = st.text_input("Company")
-year = st.number_input("Year", 1990, 2026)
-kms_driven = st.number_input("Kilometers Driven", 0)
-fuel_type = st.selectbox(
-    "Fuel Type",
-    ["Petrol", "Diesel", "CNG", "LPG"]
+st.title("🚗 Car Price Prediction Dashboard")
+
+car_name = st.selectbox(
+    "Select Car",
+    car_names
 )
 
-if st.button("Predict"):
+company = st.selectbox(
+    "Select Company",
+    companies
+)
+
+year = st.number_input(
+    "Year",
+    min_value=1995,
+    max_value=2025
+)
+
+kms_driven = st.number_input(
+    "Kilometers Driven",
+    min_value=0
+)
+
+fuel_type = st.selectbox(
+    "Fuel Type",
+    ["Petrol", "Diesel", "LPG", "CNG"]
+)
+
+if st.button("Predict Price"):
 
     sample = pd.DataFrame({
-        'name': [name],
+        'name': [car_name],
         'company': [company],
         'year': [year],
         'kms_driven': [kms_driven],
         'fuel_type': [fuel_type]
     })
 
+    st.write(sample)
     prediction = model.predict(sample)[0]
 
-    st.success(f"Estimated Price: ₹{prediction:,.0f}")
+    st.success(
+        f"Estimated Price: ₹ {prediction:,.0f}"
+    )
